0910e7a4898c7e1f3ad4b9d7cb3919fb0fca91db
deepbb <pradeepbb004@gmail.com>
2022-05-10 23:22:50 +0530

    context

--- Context/UserAction.js
+++
@@ -1,9 +0,0 @@
-export const GetAllUsers = (users)=> ({
-    type:"GET_ALLUSERS",
-    payload:users
-})
-
-export const GetUserFailed = (error)=> ({
-    type:"GET_USERFAILED",
-    payload:error
-})
